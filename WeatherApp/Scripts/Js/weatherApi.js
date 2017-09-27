00c8fc4c87fb8e5585209e8a2c27b767774013a5
akgjohansson <johansson.mcquack@gmail.com>
2017-09-27 11:14:45 +0200

    Lade till metoder för att hämta data från smhi

--- WeatherApp/Scripts/Js/weatherApi.js
+++ WeatherApp/Scripts/Js/weatherApi.js
@@ -1,1 +1,19 @@
-﻿
+﻿function BuildURL(entryPoint, longitude, latitude) {
+    var output = entryPoint;
+    var checkForEndSlash = /\/$/;
+    if (!checkForEndSlash.test(entryPoint))
+        output += '/';
+    output += `api/category/${category}/version/${version}/geotype/point/lon/${longitud}/lat/${latitude}/data.json`;
+    return output;
+}
+
+var entryPoint = "https://opendata-download-metfcst.smhi.se";
+var url = BuildURL(entryPoint, longitude, latitude);
+$("#getWeather").click(function () {
+    $.getJSON(url, function (data) {
+        console.log(data.t);
+        //location = GetLocation(data.)
+        //$("#weatherText").html(`Nu är det ${data.t} grader i ${location}`)
+    });
+});
+
